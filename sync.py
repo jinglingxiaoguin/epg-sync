70ab0a93e595e6fb43a702bdf7a578ed811beefd
jinglingxiaoguin <58650263+jinglingxiaoguin@users.noreply.github.com>
2025-10-18 00:38:35 +0800

    Add bfgd.xml.gz to SYNC_FILES list

--- sync.py
+++ sync.py
@@ -9,6 +9,7 @@ SYNC_FILES = [
     ("swh123_epg.xml.gz", "https://epgcloud.swh123.top/epg.php?ch=xml&v=e2all&s=bfgd&gz=1"),
     ("tel-epg.xml", "https://epg.deny.vip/sh/tel-epg.xml"),
     ("dm2.xml.gz", "https://epg.swh123.link:4443/live.php?ch=xml&v=dm2&gz=1"),
+    ("bfgd.xml.gz", "https://epg.swh123.link:4443/live.php?ch=xml&m=bfgd&gz=1"),
 ]
 
 
